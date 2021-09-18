1e75c5ba7875259d157a3bd3c2f55e64ea38faed
Antony Fernando <89105412+AntonyFernando3@users.noreply.github.com>
2021-09-18 17:29:41 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,104 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'CELSO RUSSOMANNO',
+                partido: 'PPS',
+                fotos:[
+                    {url: 'celso.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '00010',
+                nome: 'MARTA SUPLICY',
+                partido: 'PT',
+                fotos:[
+                    {url: 'marta.jpg', legenda:'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Maria Fernanda',
+                partido: 'Morgango TOP',
+                fotos:[
+                    {url: 'pessoa3.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '62987',
+                nome: 'Ahbdu Lah Razim',
+                partido: 'Indian Ajones',
+                fotos:[
+                    {url: 'pessoa4.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '50505',
+                nome: '50 Cent',
+                partido: 'Thug',
+                fotos:[
+                    {url: 'pessoa5.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'JOÃO DORIA',
+                partido: 'PSDB',
+                vice: 'Cic',
+                fotos:[
+                    {url:'joaodoria.jpg', legenda: 'Prefeito'},
+                    {url:'brunocovas.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'LULA SILVA',
+                partido: 'PT',
+                vice: 'JOSÉ ALENCAR',
+                fotos:[
+                    {url:'lula.jpg', legenda: 'Prefeito'},
+                    {url:'jose.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '24',
+                nome: 'Shadu',
+                partido: 'Indianos do Sul',
+                vice: 'bul',
+                fotos:[
+                    {url:'pessoa2.jpg', legenda: 'Prefeito'},
+                    {url:'pessoa8.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '19',
+                nome: 'Chun Lee',
+                partido: 'ST6',
+                vice: 'nul',
+                fotos:[
+                    {url:'pessoa2.jpg', legenda: 'Prefeito'},
+                    {url:'pessoa9.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '17',
+                nome: 'Marco Reus',
+                partido: 'BVB',
+                vice: 'tra',
+                fotos:[
+                    {url:'pessoa2.jpg', legenda: 'Prefeito'},
+                    {url:'pessoa10.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
